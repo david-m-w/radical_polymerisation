9633e09cc28ee353e1f7842e2a55b40f16712eb3
david-m-w <davidmunizwessels@gmail.com>
2025-12-22 13:25:47 +0100

    code is now in a function, and saving files works

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import matplotlib
 #print(f"Current backend: {matplotlib.get_backend()}")
 matplotlib.use('QtAgg')
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import time as t
+import os
 
 """
 viel ethen und ein paar radikale.
@@ -22,109 +25,109 @@ darstellungen:
 """
 
 
-ethen_start_anzahl = 10000000
-ethen_anzahl = ethen_start_anzahl
-radikale_start_anzahl = 10000         #sollte gerade sein, damit am ende kein radikalübrig bleibt, das kein anderen radikal findet. dies sollte kein grösseres problem sein, aber es Könnte konsequenzen haben, wenn dadurche meherere ethen moleköle an diesen radikal in der liste "offene_radikale" kleben könnten, und so aus dem system kommen
-offene_radikale = ["R"] * radikale_start_anzahl
-polyporpen_molekuele = []
+class settings:
+    seed = r.randint(0, 2**32)
+    r.seed(seed)
 
-#warschienlichkeit, dass ein radikal mit einem ethenmolekül reagiert
-ethen_reaktions_warscheinlichkeit = 1
-#warschienlichkeit, dass ein radikal mit einem anderen radikal reagiert
-radikal_reaktions_warscheinlichkeit = 1
+    ethen_start_anzahl = 1000000
+    ethen_anzahl = ethen_start_anzahl
+    radikale_start_anzahl = 1000         #sollte gerade sein, damit am ende kein radikalübrig bleibt, das kein anderen radikal findet. dies sollte kein grösseres problem sein, aber es Könnte konsequenzen haben, wenn dadurche meherere ethen moleköle an diesen radikal in der liste "offene_radikale" kleben könnten, und so aus dem system kommen
+    offene_radikale = ["R"] * radikale_start_anzahl
+    polyporpen_molekuele = []
+
+    #warschienlichkeit, dass ein radikal mit einem ethenmolekül reagiert
+    ethen_reaktions_warscheinlichkeit = 1
+    #warschienlichkeit, dass ein radikal mit einem anderen radikal reagiert
+    radikal_reaktions_warscheinlichkeit = 1
 
 
 
 
-
-def alle_radikale_gleichzeitig():
-    global ethen_anzahl, polyporpen_molekuele, offene_radikale, ethen_reaktions_warscheinlichkeit, radikal_reaktions_warscheinlichkeit
+def alle_radikale_gleichzeitig(sett):
     """ 
     in diesem versuch soll jede "generation" jedes offene radikal oder kette um 1 wachsen oder geschlossen werden
     """
-    while len(offene_radikale) > 0:
+    while len(sett.offene_radikale) > 0:
         #print(f"radikale: {len(offene_radikale)}")
         #print(f"ethen: {ethen_anzahl}")
         #print()
         
         #liste welche einträge nachher gelösht werden mussen, da ich während der schleife nicht die liste modifizieren will
-        deleted_entrys = [0] * len(offene_radikale)
-        for i in range(len(offene_radikale)):
+        deleted_entrys = [0] * len(sett.offene_radikale)
+        for i in range(len(sett.offene_radikale)):
             if deleted_entrys[i]:
                 continue
             
             #entscheiden ob dises radikal mit einem anderen radikal reagiert oder mit einem ethen:
             if (
-                r.randint(1, int(ethen_anzahl*ethen_reaktions_warscheinlichkeit + len(offene_radikale)*radikal_reaktions_warscheinlichkeit))
+                r.randint(1, int(sett.ethen_anzahl*sett.ethen_reaktions_warscheinlichkeit + len(sett.offene_radikale)*sett.radikal_reaktions_warscheinlichkeit))
                 <
-                ethen_anzahl*ethen_reaktions_warscheinlichkeit
+                sett.ethen_anzahl*sett.ethen_reaktions_warscheinlichkeit
             ):
                 #da alle ethenmoleküle  gleich sind, kann ich irgeneins nehemen
-                offene_radikale[i] += "CC"
-                ethen_anzahl -= 1
+                sett.offene_radikale[i] += "CC"
+                sett.ethen_anzahl -= 1
             else:
                 #hier muss ich aber entscheidne welches radikal:
                 #ich muss aber auch darauf achten, dass ich nicht ein radikln nehme, dass schon vorher "aufgebraucht wurde", also das schon zu einem ganzen polypropen reagiert it und kein fries elektron mehr hat, aber immerniohc in dieser liste ist, weil ich es noch nicht gelöscht habe
                 deleted_entrys[i] = 1
                 
-                radikal_partner = r.randint(0, len(offene_radikale)-1)
+                radikal_partner = r.randint(0, len(sett.offene_radikale)-1)
                 while deleted_entrys[radikal_partner]:
-                    radikal_partner = r.randint(0, len(offene_radikale)-1)
+                    radikal_partner = r.randint(0, len(sett.offene_radikale)-1)
                 
                 deleted_entrys[radikal_partner] = 1
-                polyporpen_molekuele.append(offene_radikale[i] + offene_radikale[radikal_partner][::-1])
+                sett.polyporpen_molekuele.append(sett.offene_radikale[i] + sett.offene_radikale[radikal_partner][::-1])
         
         i = 0
         while i < len(deleted_entrys):
             if deleted_entrys[i]:
-                offene_radikale.pop(i)
+                sett.offene_radikale.pop(i)
                 deleted_entrys.pop(i)
                 continue
             
             i += 1
 
-def einzeln_eins_nach_dem_anderen():
-    global ethen_anzahl, polyporpen_molekuele, offene_radikale, ethen_reaktions_warscheinlichkeit, radikal_reaktions_warscheinlichkeit
+def einzeln_eins_nach_dem_anderen(sett):
     """
     hier wird immer ein einzelnes radikal ausgewähtl und es reagiert direkt mit etwa, und dan ein anderes, und so weiter
     es wird nicht jedes radikal jede iteration gezwungen, zu reagieren
     """
     
-    while len(offene_radikale) > 0:
+    while len(sett.offene_radikale) > 0:
         
         #ein zufälliges radikal auswählen
-        i = r.randint(0, len(offene_radikale)-1)
+        i = r.randint(0, len(sett.offene_radikale)-1)
         
         #entscheiden ob dises radikal mit einem anderen radikal reagiert oder mit einem ethen:
         if (
-            r.randint(1, int(ethen_anzahl*ethen_reaktions_warscheinlichkeit + len(offene_radikale)*radikal_reaktions_warscheinlichkeit))
+            r.randint(1, int(sett.ethen_anzahl*sett.ethen_reaktions_warscheinlichkeit + len(sett.offene_radikale)*sett.radikal_reaktions_warscheinlichkeit))
             <
-            ethen_anzahl*ethen_reaktions_warscheinlichkeit
+            sett.ethen_anzahl*sett.ethen_reaktions_warscheinlichkeit
         ):
             #da alle ethenmoleküle  gleich sind, kann ich irgeneins nehemen
-            offene_radikale[i] += "CC"
-            ethen_anzahl -= 1
+            sett.offene_radikale[i] += "CC"
+            sett.ethen_anzahl -= 1
         else:
             #hier muss ich aber entscheidne welches radikal:
             #ich muss aber auch darauf achten, dass ich nicht ein radikal nehme, dass das selbe ist was ich gerade habe
-            radikal_partner = r.randint(0, len(offene_radikale)-1)
+            radikal_partner = r.randint(0, len(sett.offene_radikale)-1)
             while radikal_partner == i:
-                radikal_partner = r.randint(0, len(offene_radikale)-1)
+                radikal_partner = r.randint(0, len(sett.offene_radikale)-1)
             
-            polyporpen_molekuele.append(offene_radikale[i] + offene_radikale[radikal_partner][::-1])
+            sett.polyporpen_molekuele.append(sett.offene_radikale[i] + sett.offene_radikale[radikal_partner][::-1])
             if (i > radikal_partner):
-                offene_radikale.pop(i)
-                offene_radikale.pop(radikal_partner)
+                sett.offene_radikale.pop(i)
+                sett.offene_radikale.pop(radikal_partner)
             elif(i < radikal_partner):
-                offene_radikale.pop(radikal_partner)
-                offene_radikale.pop(i)
+                sett.offene_radikale.pop(radikal_partner)
+                sett.offene_radikale.pop(i)
             else:
                 print("das sollte nicht passieren, die sollten nie gleich sein")
                 break
 
 
-def passen_alle_elemente():
-    global polyporpen_molekuele, ethen_start_anzahl, radikale_start_anzahl, ethen_anzahl
+def passen_alle_elemente(sett):
     """
     hier wird überprüft, dass keine elemente (c oder r) zu viel existieren oder zuwenige. einfach um zu überprüfen, dass es keine bugs gibt
     
@@ -133,10 +136,10 @@ def passen_alle_elemente():
     """
     
     #die jetziege anzahl an ethenmolekühlen, die noch da sind
-    kohlenstoff_atome =  2 * ethen_anzahl
+    kohlenstoff_atome =  2 * sett.ethen_anzahl
     radikal_reste = 0
     
-    for polypropen in polyporpen_molekuele:
+    for polypropen in sett.polyporpen_molekuele:
         for atom in polypropen:
             if atom == "C":
                 kohlenstoff_atome += 1
@@ -146,23 +149,22 @@ def passen_alle_elemente():
                 print(f"ein atom wurde gefunden, dass nicht r oder c ist: {atom}")
     
     
-    if(kohlenstoff_atome != 2 * ethen_start_anzahl):
+    if(kohlenstoff_atome != 2 * sett.ethen_start_anzahl):
         print("a")
         return False
-    if(radikal_reste != radikale_start_anzahl):
+    if(radikal_reste != sett.radikale_start_anzahl):
         print("b")
         return False
     
     return True
 
 
-def laengen_analisyeren():
-    global polyporpen_molekuele, ethen_start_anzahl, radikale_start_anzahl
+def laengen_analisyeren(sett, display_results, save_results, return_results, subfolder):
     """
     hier werden die längen gezählt und ananlisiert, uns visuell dargestellt
     """
     
-    laengen = map(len, polyporpen_molekuele)
+    laengen = map(len, sett.polyporpen_molekuele)
     laengen_counts = dict(Counter(laengen))
     laengen_counts_keys = list(laengen_counts.keys())
     laengen_counts_values = [laengen_counts[key] for key in laengen_counts_keys]
@@ -170,15 +172,58 @@ def laengen_analisyeren():
     print(laengen_counts_keys)
     print(laengen_counts_values)
     
-    plt.plot(laengen_counts_keys, laengen_counts_values)
-    plt.show()
+    #saw this lien somwhere, but it dosent seem necesssary
+    fig = plt.figure(figsize=(20, 12))
+    gs = gridspec.GridSpec(2, 1)
+    
+    ax_scatter = plt.subplot(gs[1:2, :2])
+    ax_scatter.scatter(laengen_counts_keys, laengen_counts_values)
+    ax_scatter.set(
+        xlabel="länge der kette",
+        ylabel="häufigkeit der länge",
+        title = f"es it noch {sett.ethen_anzahl} übrig, also {sett.ethen_anzahl/sett.ethen_start_anzahl * 100}%\nseed: {sett.seed}\nethen start anzahl: {sett.ethen_start_anzahl}\nradikale start anzahl: {sett.radikale_start_anzahl}"
+    )
+
+    ax_hist = plt.subplot(gs[0, :2],sharex=ax_scatter)
+    ax_hist.hist(laengen_counts_keys, bins = 100)
+    ax_hist.set(
+        xlabel="länge der kette",
+        ylabel="häufigkeit der länge"#,
+        #title = f"es it noch {ethen_anzahl} übrig, also {ethen_anzahl/ethen_start_anzahl * 100}%\nseed: {seed}"
+    )
+    
+    plt.tight_layout()
+    
+    if save_results:
+        if not os.path.isdir(f"plot_saves/{subfolder}"):
+            os.makedirs(f"plot_saves/{subfolder}")
+        plt.savefig(f"plot_saves/{subfolder}/image_{sett.seed}_{sett.ethen_start_anzahl}_{sett.radikale_start_anzahl}.svg", format="svg")
+    if display_results:
+        plt.show()
+    if return_results:
+        return (laengen_counts_keys, laengen_counts_values)
+    else:
+        return None
 
 
-einzeln_eins_nach_dem_anderen()
+def main(alle_zusammen, einzeln, display_results, save_results, return_results, subfolder):    
+    sett = settings()
+    
+    if alle_zusammen:
+        alle_radikale_gleichzeitig(sett)
+    if einzeln:
+        einzeln_eins_nach_dem_anderen(sett)
+    
+    if not passen_alle_elemente(sett):
+        print(f"error on seed: {sett.seed}")
+        raise Exception
+    
+    res = laengen_analisyeren(sett, display_results, save_results, return_results, subfolder)
+    
+    if return_results:
+        return res
 
-if not passen_alle_elemente():
-    raise Exception
-
-laengen_analisyeren()
-
-raise "wenn die diagramme nicht klar genug sind, könnte man die simualation ofters machen und den durschschnitt nehmen"
+if __name__ == "__main__":
+    main(True, False, True, True, False, f"single_{int(t.time())}")
+#else:
+    #main(True, False, False, True, True, f"multi_{t.time()}")
